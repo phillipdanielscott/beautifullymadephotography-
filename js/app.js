9f9ae40eb053423449db4e9ce333a83567d9b75b
phillipdanielscott <phillipd20@yahoo.com>
2016-09-01 15:35:50 -0400

    fixing routes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 var app = angular.module("photoApp", ['ngRoute'])
 
 app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    .when('/', {
+  $routeProvider.when('/', {
+      templateUrl: 'templates/bio.html',
       controller: 'MainController',
-      templateUrl: 'templates/portfolio.html'
+    }).when('/bio',{
+      termplateURL:'templates/bio.html',
+      controller:'MainController'
     })
 }])
 
-// require('./controllers/MainController.js')(app);
+require('./js/controllers/MainController.js')(app);
