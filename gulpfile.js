d49b0f1af248c30dbc9d454a55f8ccf49d0e39ad
phillipdanielscott <phillipd20@yahoo.com>
2016-09-02 15:16:13 -0400

    getting back on the horse...things got deleted.:(

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+// var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
+
+gulp.task('default', ['html', 'js', 'css','watch']);
+
+
+//html
+gulp.task('html', function(){
+  // gulp.src('./templates/*.html')
+  //   .pipe(gulp.dest('./public/templates'))
+    gulp.src('./templates/*.html').pipe(gulp.dest('./public/templates'));
+    // Also copy over the templates directory.
+
+
+  return gulp.src('./index.html')
+    .pipe(gulp.dest('./public'));
+});
+
+//js
+gulp.task('js', function(){
+  gulp.src('/js/app.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('./public'));
+});
+
+
+//css
+gulp.task('css', function(){
+  gulp.src('./scss/styles.scss')
+    // .pipe(sass())
+    .pipe(gulp.dest('./public/css'));
+})
+
+gulp.task('watch', function(){
+  gulp.watch('./index.html', ['html'])
+  gulp.watch('./js/*.js', ['js'])
+  gulp.watch('./templates/*.html', ['html'])
+  gulp.watch('./scss/*.scss', ['css'])
+});
---
+++ js/controllers/MainController.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+app.controller('MainController', ['$scope', function($scope) {
+
+
+console.log("we are farmers, dun udn udnundund")
+
+
+
+
+
+
+
+
+}]);
+
+}
